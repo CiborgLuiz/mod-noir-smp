0a7294e13b0cf9d457c196fe771fc104d79e33ab
CiborgLuiz <jamillajaninenutri@gmail.com>
2026-02-17 19:18:22 -0300

    Ajustando aleatoriedade do dado

--- src/main/java/net/mcreator/noirsmp/procedures/DadoFeuralRightclickedProcedure.java
+++ src/main/java/net/mcreator/noirsmp/procedures/DadoFeuralRightclickedProcedure.java
@@ -11,24 +11,44 @@ public class DadoFeuralRightclickedProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (Mth.nextInt(RandomSource.create(), 1, 6) == 1) {
+		double dado = 0;
+		dado = Mth.nextInt(RandomSource.create(), 1, 6);
+		if (dado == 1) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
-		} else if (Mth.nextInt(RandomSource.create(), 1, 6) == 2) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 60, 3));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
-		} else if (Mth.nextInt(RandomSource.create(), 1, 6) == 3) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 500, 1));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
-		} else if (Mth.nextInt(RandomSource.create(), 1, 6) == 4) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.UNLUCK, 500, 4));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
-		} else if (Mth.nextInt(RandomSource.create(), 1, 6) == 5) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 500, 2));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
-		} else if (Mth.nextInt(RandomSource.create(), 1, 6) == 6) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 500, 6));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 60, 1));
+				_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 500, 10));
+		} else if (dado == 2) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 300, 1));
+		} else if (dado == 3) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.HUNGER, 100, 4));
+		} else if (dado == 4) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.LUCK, 300, 2));
+		} else if (dado == 5) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 40, 2));
+		} else if (dado == 6) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 60, 2));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 500, 3));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 500, 2));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 60, 5));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 500, 5));
 		}
 	}
 }
