e4cdb60964e7f6abe4ab541fe878b06f1604a1b6
CiborgLuiz <jamillajaninenutri@gmail.com>
2026-02-19 19:59:41 -0300

    Depois de insistencia em base de pura determinação e um forte senso de continuar, finalmente o problema foi mitigado e o reino do ceu está cada vez mais proximo, a realidade está colaborando com a existencia de ser sofredor que passou por todos os males possiveis e conseguiu alcançar o sucesso momentaneo conseguindo completar seu objetivo, eu (CiborgLuiz), fico satisfeito ao conseguir arrumar a foice de auto autoria, e todos os outros modelos foram concertados, Obrigado Danilo ao conquistar o ceu vendo aquilo que todos exitavam olhar, você é o mais honrado, você salvou nossas almas pobres e miseraveis

--- src/main/java/net/mcreator/noirsmp/item/ESPADAItem.java
+++ src/main/java/net/mcreator/noirsmp/item/ESPADAItem.java
@@ -18,11 +18,16 @@ public class ESPADAItem extends Item {
 	}
 
 	@Override
+	public int getEnchantmentValue() {
+		return 20;
+	}
+
+	@Override
 	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot equipmentSlot) {
 		if (equipmentSlot == EquipmentSlot.MAINHAND) {
 			ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
 			builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
-			builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Item modifier", 14d, AttributeModifier.Operation.ADDITION));
+			builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Item modifier", 17d, AttributeModifier.Operation.ADDITION));
 			builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Item modifier", -2.4, AttributeModifier.Operation.ADDITION));
 			return builder.build();
 		}
